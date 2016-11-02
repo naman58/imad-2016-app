8b13936a13355e6b9a76774315db1208fa50fc90
naman58 <narula.naman5@gmail.com>
2016-11-02 22:16:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text')
 element.innerHTML = 'new VALUE'
+//move the image 
+var img=document.getElementById('img');
+img.onclick = function() {
+    img.style.increaseleft='1000px';
+}
