4c20604b6e4f2bfc921e3bc9240594cfcea383a5
naman58 <narula.naman5@gmail.com>
2016-11-02 22:19:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text')
 element.innerHTML = 'new VALUE'
 //move the image 
-var img=document.getElementById('img');
+var img=document.getElementById('MADI');
 img.onclick = function() {
     img.style.marginleft='1000px';
 }
