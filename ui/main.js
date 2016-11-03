b0fac8b06ca969cdb6023f67508b8305506ad576
naman58 <narula.naman5@gmail.com>
2016-11-03 13:43:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,23 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function() {
-    //make a request to the counter endpoint
+    //create a request object
+    var request = new XMLHttpRequest();
     
+
     //capture the response and store it in a variable
-    
-    //render the variable in the correct span
-    
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function() {
+        if (request.readystate ===XMLHttpRequest.DONE) {
+            //take some action
+            if (request.status === 200) {
+              var cuonter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    };
+    //make a request
+    request.open('GET','http://naman58.imad.hasura-app.io/counter',true);
+    request.send(null);
 };  
