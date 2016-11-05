cba99610b50199881363bc24af538ba4bad48eb7
naman58 <narula.naman5@gmail.com>
2016-11-05 19:40:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+    user: 'naman58',
+    database: 'naman58',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -86,7 +93,19 @@ app.get('/:articleName', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
+var Pool = require('pg').Pool;
+app.get('/db', function(req,res){
+   //make a request
+   //return a response with the results
+   pool.query('SELECT * FROM test', function(err,result){
+       if (err){
+           res.status(500).send(err.toString());
+       }else{
+           res.send(JSON.stringify(result));
+           
+       }
+   });
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
