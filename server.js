ed708f06dc2ebd0d86445915694a1eed9dc2aa76
naman58 <narula.naman5@gmail.com>
2016-11-05 19:41:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var Pool = require('pg').Pool;
-app.get('/db', function(req,res){
+app.get('/test-db', function(req,res){
    //make a request
    //return a response with the results
    pool.query('SELECT * FROM test', function(err,result){
